65e493f144a52b973d7cf64fb74cf76d0f9f23d2
Damir Korencic <damir.korencic@gmail.com>
2014-02-11 11:24:09 +0100

    new JRI.jar version

--- config/maven_install_local_jar.sh
+++ config/maven_install_local_jar.sh
@@ -1,16 +1,17 @@
 MAVEN="/home/dam1root/netbeans-7.3.1/java/maven/bin/mvn";
 
 # Trove
-GROUP_ID="gnu.trove";
-ART_ID="gnu.trove.all";
-VERSION="3.0.3";
-LOCATION="/home/dam1root/software/trove-3.0.3/3.0.3/lib/trove-3.0.3.jar"
+#GROUP_ID="gnu.trove";
+#ART_ID="gnu.trove.all";
+#VERSION="3.0.3";
+#LOCATION="/home/dam1root/software/trove-3.0.3/3.0.3/lib/trove-3.0.3.jar"
 
 # rJava
-#GROUP_ID="rJava";
-#ART_ID="JRI.jar";
-#VERSION="0.5-0";
-#LOCATION="/home/dam1root/R/x86_64-pc-linux-gnu-library/2.14/rJava/jri/JRI.jar";
+GROUP_ID="rJava";
+ART_ID="JRI.jar";
+VERSION="0.5-0";
+LOCATION="/home/dam1root/R/x86_64-pc-linux-gnu-library/3.0/rJava/jri/JRI.jar";
 
 $MAVEN install:install-file -DgroupId=$GROUP_ID -DartifactId=$ART_ID \
  -Dpackaging=jar -Dversion=$VERSION -Dfile=$LOCATION
+

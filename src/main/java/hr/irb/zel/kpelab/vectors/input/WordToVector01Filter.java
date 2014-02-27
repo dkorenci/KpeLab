291a46fa76f5386be328311103e32f0d0561ea06
dam1root <dam1root@batou>
2014-02-27 18:03:39 +0100

    added WordToVector01Filter

---
+++ src/main/java/hr/irb/zel/kpelab/vectors/input/WordToVector01Filter.java
@@ -0,0 +1,45 @@
+package hr.irb.zel.kpelab.vectors.input;
+
+import hr.irb.zel.kpelab.vectors.IRealVector;
+import hr.irb.zel.kpelab.vectors.VectorEntry;
+
+/** Coverts vector from the map to 0-1 vectors that have 
+ * value of 1 at all coordinates !=0. */
+public class WordToVector01Filter implements IWordToVectorMap {
+
+    IWordToVectorMap wordToVector;
+    WordToVectorMapCache cache;
+    private static final int CACHE_SIZE = 10000;
+    
+    public WordToVector01Filter(IWordToVectorMap wvm) { 
+        wordToVector = wvm; 
+        cache = new WordToVectorMapCache(CACHE_SIZE);
+    }
+    
+    public IRealVector getWordVector(String word) throws Exception {
+        if (cache.hasWord(word)) return cache.getWordVector(word);
+        else {
+            if (wordToVector.hasWord(word)) {
+                IRealVector vec = wordToVector.getWordVector(word);
+                IRealVector vec01 = create01Vector(vec);
+                cache.addWordVectorPair(word, vec01);
+                return vec01;
+            }
+            else return null;
+        }
+    }
+
+    public boolean hasWord(String word) {
+        return wordToVector.hasWord(word);
+    }
+
+    private IRealVector create01Vector(IRealVector v) {
+        VectorEntry[] entries = v.getNonZeroEntries(); 
+        IRealVector result = v.clone();
+        for (VectorEntry e : entries) {
+            result.setElement(e.coordinate, 1);
+        }
+        return result;
+    }
+    
+}
--- src/main/java/hr/irb/zel/kpelab/vectors/input/WordVectorMapFactory.java
+++ src/main/java/hr/irb/zel/kpelab/vectors/input/WordVectorMapFactory.java
@@ -6,6 +6,7 @@ public class WordVectorMapFactory {
 
     private static WordToVectorDiskMap wikiLSIvectors;
     private static WordToVectorDiskMap wikiESAvectors;
+    private static WordToVector01Filter wikiESA01vectors;
     
     public static WordToVectorMemMap getCwEmbeddings() throws Exception {
         WordToVectorMemMap wvm = new WordToVectorMemMap(
@@ -31,6 +32,13 @@ public class WordVectorMapFactory {
         return wikiESAvectors;
     }    
     
+    public static WordToVector01Filter getESA01Vectors() throws Exception {
+        if (wikiESA01vectors == null) {            
+            wikiESA01vectors = new WordToVector01Filter(getESAVectors());
+        }
+        return wikiESA01vectors;
+    }    
+        
     /** Perform any actions necessary before the application ends. */
     public static void closeFactory() throws Exception {
         if (wikiLSIvectors != null) { wikiLSIvectors.saveCache(); }

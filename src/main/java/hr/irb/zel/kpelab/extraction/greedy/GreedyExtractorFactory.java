c98efe38df05e057f6abb16ea410be6c256b8ab1
dam1root <dam1root@batou>
2014-03-03 11:36:54 +0100

    added automatic generation of a configuration based on options

--- src/main/java/hr/irb/zel/kpelab/extraction/greedy/GreedyExtractorFactory.java
+++ src/main/java/hr/irb/zel/kpelab/extraction/greedy/GreedyExtractorFactory.java
@@ -37,6 +37,95 @@ public class GreedyExtractorFactory {
         return exts;
     }
     
+    // options for configuration creation
+    public enum Vec { LSI, ESA }
+    public enum VecMod {
+        NONE, // no modification
+        ZERO_ONE, // turn non zero coordinates to 1
+        PRUNE // prune unshared coordinates in a document
+    }
+    // document to vector aggregation method
+    public enum DocAgg {
+        TF_SUM, // sum content words mult. by tf
+        TFIDF_SUM // sum content words mult. by tfidf
+    }
+    // phrase set to vector aggregation method
+    public enum PhAgg { 
+        UW_SUM // sum of vectors of unique words
+    }
+    // quality measure that compares phrase set and document vectors
+    public enum VecQ { COS, COS_CUT, EBE }
+    
+    // creates extractor config based on options
+    public static GreedyExtractorConfig create(Vec vec, boolean vec01, boolean vecPr, 
+            DocAgg doc, PhAgg ph, VecQ vecq) throws Exception {
+        // canonic form
+        CanonicForm cform;
+        if (vec == Vec.LSI) cform = CanonicForm.LEMMA;
+        else if (vec == Vec.ESA) cform  = CanonicForm.STEM;
+        else throw new UnsupportedOperationException(); 
+        
+        // phrase extractor
+        IPhraseExtractor phext = new PosRegexPhraseExtractor(
+                new PosExtractorConfig(Components.OPEN_NLP, cform));    
+        
+        // word to vector mapping
+        IWordToVectorMap wvm;
+        if (vec == Vec.LSI) {
+            wvm = WordVectorMapFactory.getLSIVectors();
+        }
+        else if (vec == Vec.ESA) {
+            if (vec01) wvm = WordVectorMapFactory.getESA01Vectors();
+            else wvm = WordVectorMapFactory.getESAVectors();
+            
+        }
+        else throw new UnsupportedOperationException();         
+        // "local Wvm", for doc aggregation and phrase aggregation components
+        // set to null if vector prunning is on, to capture potential bugs
+        // with initialization, since word to vector maps for these components
+        // have to be initialized before each document processing        
+        IWordToVectorMap compWvm;
+        if (vecPr) compWvm = null; else compWvm = wvm;        
+        
+        // document vectorization
+        IDocumentVectorizer dvec;
+        if (doc == DocAgg.TFIDF_SUM) {   
+            TermDocumentFrequency tdf = DfFactory.loadDfSemevalStemOpenNlp();
+            dvec = new TfIdfVectorizer(compWvm, cform, tdf);
+        }
+        else if (doc == DocAgg.TF_SUM) {
+            dvec = new TermFrequencyVectorizer(compWvm, cform);
+        }
+        else throw new UnsupportedOperationException(); 
+        
+        // phrase set vectorization
+        IPhraseSetVectorizer phvec;
+        if (ph == PhAgg.UW_SUM) {
+            phvec = new SumPhraseSetVectorizer(wvm);
+        }
+        else throw new UnsupportedOperationException();
+        
+        // vector comparison
+        IVectorComparison cmp;        
+        if (vecq == VecQ.COS) {
+           cmp = new VectorSimilarity(SimilarityMeasure.COSINE);  
+        }
+        else if (vecq == VecQ.COS_CUT) {
+            cmp = new VectorSimilarity(SimilarityMeasure.COSINE_CUTOFF);  
+        }
+        else if (vecq == VecQ.EBE) {
+            cmp = new VectorSimilarity(SimilarityMeasure.EBE_MULTIPLY);  
+        }
+        else throw new UnsupportedOperationException(); 
+        
+        // vector modification flag
+        VectorMod vmod;
+        if (vecPr == false) vmod = VectorMod.NONE;
+        else vmod = VectorMod.PRUNE_UNIQUE;
+                    
+        return new GreedyExtractorConfig(wvm, vmod, cform, dvec, phext, phvec, cmp);        
+    }
+    
     public static GreedyExtractorConfig getLSICosExtractor() throws Exception {
         CanonicForm cform = CanonicForm.LEMMA;
         IWordToVectorMap wvm = WordVectorMapFactory.getLSIVectors();

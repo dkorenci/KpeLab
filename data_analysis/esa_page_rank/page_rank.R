b8ada60c3cb6b2b5830b3780e331fece87a46516
dam1root <dam1root@batou>
2014-03-04 20:59:43 +0100

    damping factor added as parameter

--- data_analysis/esa_page_rank/page_rank.R
+++ data_analysis/esa_page_rank/page_rank.R
@@ -3,17 +3,19 @@
 # N - size of the graph matrix
 # flatMatrix - weighted adjacency matrix as numeric N*N vector, row by row
 # persRank - personalized rank values, vector of size N
+# dampingF - damping factor
 # returns: numeric vector of ranks assigned to variable result
 
 # test: 
 # N = 10
 # flatMatrix = rep(1, N*N)
 # persRank = rep(1, N)
+# dampingF = 0.85
 
 library("igraph")
 adjMatrix <- matrix(flatMatrix,nrow=N,ncol=N,byrow=T)
 g <- graph.adjacency(adjMatrix, mode="undirected", weighted=TRUE, diag=FALSE,
                       add.colnames=NA, add.rownames=NA)
-pr <- page.rank(g, directed=FALSE, personalized=persRank)
+pr <- page.rank(g, directed=FALSE, personalized=persRank,damping=dampingF)
 print("hi")
 result <- pr$vector
